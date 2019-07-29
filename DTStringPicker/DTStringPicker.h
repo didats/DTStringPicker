d853773865375301a52faeaadbb5dcc06bfac263
didats <didats@gmail.com>
2019-07-30 05:27:49 +0700

    Added project files

---
+++ DTStringPicker/DTStringPicker.h
@@ -0,0 +1,19 @@
+//
+//  DTStringPicker.h
+//  DTStringPicker
+//
+//  Created by Didats Triadi on 30/07/19.
+//  Copyright © 2019 Rimbunesia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DTStringPicker.
+FOUNDATION_EXPORT double DTStringPickerVersionNumber;
+
+//! Project version string for DTStringPicker.
+FOUNDATION_EXPORT const unsigned char DTStringPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DTStringPicker/PublicHeader.h>
+
+
